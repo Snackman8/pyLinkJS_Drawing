bf62cedb9e098fa7c7415175b10c9e318b5f31fe
Snackman8 <lawy888@gmail.com>
2023-07-16 19:42:42 -0700

    refactor to make layer apps easier

--- examples/pyLinkJS_Drawing_example.py
+++ examples/pyLinkJS_Drawing_example.py
@@ -3,138 +3,41 @@
 # --------------------------------------------------
 import argparse
 import datetime
+import os
 import random
-import threading
+import time
 import logging
 import pandas as pd
-from pylinkjs.PyLinkJS import run_pylinkjs_app, get_broadcast_jsclients
-from pyLinkJS_Drawing.drawingPlugin import *
-from pyLinkJS_Drawing.layerController import LayerController, LayerDataSource, LayerRenderer
+from pylinkjs.PyLinkJS import run_pylinkjs_app
+from pyLinkJS_Drawing.drawingPlugin import pluginDrawing, CircleObject, RectObject, TextObject
+from pyLinkJS_Drawing.layerController import LayerDataSource, LayerRenderer, LayerApp
 
 
 # --------------------------------------------------
 #    Globals
 # --------------------------------------------------
-DATA_CLICK_PREFIX = 'H'
-DATA_CLICK_INDEX = 9
-LAYER_CONTROLLER = None
-
-
-# --------------------------------------------------
-#    Function
-# --------------------------------------------------
-def compute_image_scale(w, h):
-    if w < h:
-        scale = 2000.0 / h
-    else:
-        scale = 2000.0 / w
-
-    return (w * scale, h * scale)
-
-
-def compute_zoom(canvas_width, canvas_height, image_width, image_height):
-    c_ar = canvas_width / canvas_height
-    i_ar = image_width / image_height
-
-    if c_ar < i_ar:
-        # fit to image width
-        return canvas_width / image_width
-    else:
-        # fit to image height
-        return canvas_height / image_height
+LAYER_APP = None
 
 
 # --------------------------------------------------
 #    Event Handlers
 # --------------------------------------------------
 def onmouseup(jsc, x, y, button):
-    """ print out the click coordinate """
-    global DATA_CLICK_INDEX
-
-    if (button == 0):
-        print(f'{DATA_CLICK_PREFIX}-{DATA_CLICK_INDEX},{int(x)},{int(y)}')
-        DATA_CLICK_INDEX = DATA_CLICK_INDEX + 1
-
-        if 'ROOT_RENDER_OBJECT' in jsc.tag:
-            # check if we need to move the tool tip
-            mlp = jsc.eval_js_code(f"""mouse_get_last_position();""")
-            if mlp is not None:
-                t = time.time()
-                rolist = jsc.tag['ROOT_RENDER_OBJECT'].point_in_obj(mlp['wx'], mlp['wy'], t)
-
-
-                # assemble a layer list
-                tooltip_idx = None
-                layer_names = set()
-                for ro in rolist:
-                    if 'layer_name' in ro.props:
-                        layer_names.add(ro.props['layer_name'])
-                        if 'idx' in ro.props:
-                            tooltip_idx = ro.props['idx']
-
-                jsc.tag['property_window'] = {'layer_names': layer_names, 'idx': tooltip_idx}
-
-                html = LAYER_CONTROLLER.get_tooltip(layer_names, tooltip_idx)
-                jsc['#properties'].html = html
+#    return LAYER_APP.on_mouseup(jsc, x, y, button, LAYER_CONTROLLER)
+    return LAYER_APP.on_mouseup(jsc, x, y, button)
 
 
 def options_changed(jsc, *args):
-    LAYER_CONTROLLER.update_options(jsc)
+    return LAYER_APP.on_options_changed(jsc)
 
 
 def ready(jsc, *args):
-    """ called when a webpage creates a new connection the first time on load """
-    # create the render objects list
-    f = jsc.drawing()
-    f.create_image('img_floor_plan', jsc.tag['background_file'])
-    f.render(jsc)
-
-    # get the canvas width and canvas height
-    canvas_width = jsc.eval_js_code('ctx1.canvas.width')
-    canvas_height = jsc.eval_js_code('ctx1.canvas.height')
-
-    # wait for image to load
-    img_width, img_height = canvas_width, canvas_height
-    start_time = time.time()
-    while (time.time() - start_time) < 5:
-        if jsc.eval_js_code('img_floor_plan.complete'):
-            img_width = jsc.eval_js_code('img_floor_plan.naturalWidth')
-            img_height = jsc.eval_js_code('img_floor_plan.naturalHeight')
-            break
-
-    # attempt to scale the image up to a normalized coordinate system of 2000 x 2000
-    img_width, img_height = compute_image_scale(img_width, img_height)
-
-    # calcualte zoom
-    zoom = compute_zoom(canvas_width, canvas_height, img_width, img_height)
-
-    # set the initial zoom to fit the image
-    jsc.eval_js_code(f"""force_zoom('Canvas1', 'Canvas2', 0, 0, {zoom});""")
-    jsc.eval_js_code(f"""force_translate('Canvas1', 'Canvas2', {(canvas_width / zoom - img_width) / 2}, {(canvas_height / zoom - img_height) / 2});""")
-
-    # create the background
-    root_obj = RectObject(flightplan=None, x=0, y=0, width=canvas_width, height=canvas_height, strokeStyle='rgba(0, 0, 0, 0)', fillStyle='rgba(0, 0, 0, 0)', clickable=False)
-
-    # create the background
-    image_obj = ImageObject(name='img', flightplan=None, x=0, y=0, width=img_width, height=img_height, image_name='img_floor_plan', filter_str='opacity(0.2)')
-    root_obj.add_child(image_obj)
-    jsc.tag['ROOT_RENDER_OBJECT'] = root_obj
-
-    # initialize the layer renderers for this jsc
-    for dr in LAYER_CONTROLLER._layer_datarenderers.values():
-        dr.layer_init(image_obj)
-
-    # generate html for layer selection
-    jsc['#options'].html = LAYER_CONTROLLER.build_options_html(jsc)
-    LAYER_CONTROLLER.update_options(jsc)
-
-    # render
-    f.render(jsc)
+#    return LAYER_APP.on_ready(jsc, jsc.tag['background_file'], 'ctx_drawing', 'ctx_display', LAYER_CONTROLLER)
+    return LAYER_APP.on_ready(jsc, jsc.tag['background_file'], 'ctx_drawing', 'ctx_display')
 
 
 def reconnect(jsc, *args):
     """ called when a webpage automatically reconnects a broken connection """
-    print('Reconnect', args)
     ready(jsc)
 
 
@@ -208,14 +111,7 @@ class LR_Example_Circle(LayerRenderer):
         return [{'id': 'circle', 'text': 'Example Circle', 'type': 'Boolean', 'default_value': True}]
 
     def get_tooltip(self, tooltip_idx):
-#        idx = None
-        html = ''
-        # for ro in rolist:
-        #     if 'idx' in ro.props:
-        #         idx = ro.props['idx']
-        html += 'Radius: ' + str(self._data.loc[tooltip_idx].circle_radius) + '<br>'
-#                break
-        return html
+        return 'Radius: ' + str(self._data.loc[tooltip_idx].circle_radius) + '<br>'
 
     def on_data_changed(self, data_dict):
         # merge the data
@@ -263,11 +159,7 @@ class LR_Example_Text(LayerRenderer):
                 {'id': 'always_red', 'text': 'Always Red', 'type': 'Boolean', 'default_value': False}]
 
     def get_tooltip(self, tooltip_idx):
-        html = ''
-        html += tooltip_idx + '<br>'
-        html += 'Value: ' + str(self._data.loc[tooltip_idx].Value) + '<br>'
-        html += 'TimeStamp: ' + str(self._data.loc[tooltip_idx].Value_ts) + '<br>'
-        return html
+        return tooltip_idx + '<br>Value: ' + str(self._data.loc[tooltip_idx].Value) + '<br>TimeStamp: ' + str(self._data.loc[tooltip_idx].Value_ts) + '<br>'
 
     def on_data_changed(self, data_dict):
         # merge the data
@@ -314,123 +206,30 @@ class LR_Example_Text(LayerRenderer):
 # --------------------------------------------------
 #    Main
 # --------------------------------------------------
-def start_threaded_automatic_update():
-    """ example of how to run a periodic update from a different thread """
-    def thread_worker():
-        # init
-        last_render_refresh_time = 0
-        last_status_refresh_time = 0
-        last_tooltip_check_time = 0
-        last_property_refresh_time = 0
-
-        # loop forever
-        while True:
-            t = time.time()
-
-            # refresh the data visually if needed
-            if (t - last_render_refresh_time) > 0.1:
-                for jsc in get_broadcast_jsclients('/'):
-
-                    # refresh the render objects associated with the data
-                    if 'ROOT_RENDER_OBJECT' in jsc.tag:
-                        image_obj = jsc.tag['ROOT_RENDER_OBJECT'].children['img']
-                        LAYER_CONTROLLER.render(image_obj, jsc.tag['options'])
-
-                        f = JSDraw('ctx1', 'ctx2')
-                        f.fillStyle = 'white'
-                        f.clear()
-                        jsc.tag['ROOT_RENDER_OBJECT'].render(f, t)
-                        f.render(jsc)
-                last_render_refresh_time = time.time()
-
-
-            if (t - last_tooltip_check_time) > 0.1:
-                for jsc in get_broadcast_jsclients('/'):
-                    if 'ROOT_RENDER_OBJECT' in jsc.tag:
-                        # check if we need to move the tool tip
-                        mlp = jsc.eval_js_code(f"""mouse_get_last_position();""")
-                        if mlp is not None:
-                            if mlp['elapsed_ms'] > 500:
-                                t = time.time()
-                                rolist = jsc.tag['ROOT_RENDER_OBJECT'].point_in_obj(mlp['wx'], mlp['wy'], t)
-
-                                # assemble a layer list
-                                tooltip_idx = None
-                                layer_names = set()
-                                for ro in rolist:
-                                    if 'layer_name' in ro.props:
-                                        layer_names.add(ro.props['layer_name'])
-                                        if 'idx' in ro.props:
-                                            tooltip_idx = ro.props['idx']
-
-                                html = LAYER_CONTROLLER.get_tooltip(layer_names, tooltip_idx)
-                                if html != '':
-                                    jsc['#tooltip'].html = html
-                                    jsc['#tooltip'].css.left = mlp['px']
-                                    jsc['#tooltip'].css.top = mlp['py']
-                                    jsc['#tooltip'].css.visibility = 'visible'
-                                    jsc['body'].css.cursor = 'crosshair'
-                                else:
-                                    jsc['#tooltip'].css.visibility = 'hidden'
-                                    jsc['body'].css.cursor = 'default'
-                            else:
-                                jsc['#tooltip'].css.visibility = 'hidden'
-                                jsc['body'].css.cursor = 'default'
-                last_tooltip_check_time = time.time()
-
-
-            # refresh property window if needed
-            if (t - last_property_refresh_time) > 1:
-                for jsc in get_broadcast_jsclients('/'):
-                    if 'property_window' in jsc.tag:
-                        layer_names = jsc.tag['property_window']['layer_names']
-                        idx = jsc.tag['property_window']['idx']
-                        html = LAYER_CONTROLLER.get_tooltip(layer_names, idx)
-                        jsc['#properties'].html = html
-
-            # refresh the status if needed
-            if (t - last_status_refresh_time) > 1:
-                for jsc in get_broadcast_jsclients('/'):
-                    jsc['#datasources'].html = LAYER_CONTROLLER.get_datasource_status_messages()
-                last_status_refresh_time = time.time()
-
-            # sleep 10ms
-            time.sleep(0.01)
-
-    # start the thread
-    t = threading.Thread(target=thread_worker, daemon=True)
-    t.start()
-
-
-# --------------------------------------------------
-#    Main
-# --------------------------------------------------
 def main(args):
-    global LAYER_CONTROLLER
+    # globals
+    global LAYER_APP
 
-    # init the google oauth2 plugin
-    drawing_plugin = pluginDrawing('ctx1', 'ctx2')
-
-    LAYER_CONTROLLER = LayerController(minimum_datasource_cooldown_period=3)
-
-    # read the data coordinate file
+    # data_coords
     data_coord_file = os.path.join(os.path.dirname(__file__), args['data_coordinate_file'])
     df_data_coords = pd.read_csv(data_coord_file, index_col=0)
     lds_data_coords = LayerDataSource(name='data_coords', next_fire_time=None, initial_data=df_data_coords)
+    # Example Values
     lds_value = LDS_Example_Values(cooldown_period=1)
+    # Example Open
     lds_open = LDS_Example_Open(cooldown_period=1)
-    LAYER_CONTROLLER._layer_datasources[lds_data_coords.name] = lds_data_coords
-    LAYER_CONTROLLER._layer_datasources[lds_value.name] = lds_value
-    LAYER_CONTROLLER._layer_datasources[lds_open.name] = lds_open
 
-    lr_circle = LR_Example_Circle(name='circle', starting_data_dict={'data_coords': df_data_coords}, subscribed_datasources=[lds_data_coords.name, lds_value.name, lds_open.name] )
+    # text
     lr_text = LR_Example_Text(name='text', starting_data_dict={'data_coords': df_data_coords}, subscribed_datasources=[lds_data_coords.name, lds_value.name, lds_open.name] )
-    LAYER_CONTROLLER._layer_datarenderers[lr_circle.name] = lr_circle
-    LAYER_CONTROLLER._layer_datarenderers[lr_text.name] = lr_text
+    # circle
+    lr_circle = LR_Example_Circle(name='circle', starting_data_dict={'data_coords': df_data_coords}, subscribed_datasources=[lds_data_coords.name, lds_value.name, lds_open.name] )
 
-    LAYER_CONTROLLER.start()
+    # initialize the LAYER_APP
+    LAYER_APP = LayerApp(data_sources=[lds_data_coords, lds_value, lds_open], renderers=[lr_text, lr_circle])
+    LAYER_APP.start()
 
-    start_threaded_automatic_update()
+    # initialize the app
+    drawing_plugin = pluginDrawing('ctx_drawing', 'ctx_display')
     run_pylinkjs_app(default_html='pyLinkJS_Drawing_example.html', extra_settings={'background_file': args['background_file'], 'data_coordinates_file': args['data_coordinate_file']}, plugins=[drawing_plugin])
 
 
--- pyLinkJS_Drawing/layerController.py
+++ pyLinkJS_Drawing/layerController.py
@@ -8,6 +8,8 @@ import threading
 import time
 import traceback
 import pandas as pd
+from pyLinkJS_Drawing.drawingPlugin import JSDraw, ImageObject, RectObject
+from pylinkjs.PyLinkJS import get_broadcast_jsclients
 
 
 # --------------------------------------------------
@@ -196,7 +198,7 @@ class LayerController:
                 try:
                     html += self._layer_datarenderers[name].get_tooltip(tooltip_idx)
                 except Exception as e:
-                    html += repr(e) + '<br>' 
+                    html += repr(e) + '<br>'
         return html
 
     def render(self, parentObj, options):
@@ -275,3 +277,202 @@ class LayerController:
 
                 # one second frequency
                 time.sleep(1)
+
+
+class LayerApp:
+    def __init__(self, data_sources, renderers):
+        self.layer_controller = LayerController(minimum_datasource_cooldown_period=3)
+
+        for lds in data_sources:
+            self.layer_controller._layer_datasources[lds.name] = lds
+
+        for lr in renderers:
+            self.layer_controller._layer_datarenderers[lr.name] = lr
+
+        self.layer_controller.start()
+
+    @classmethod
+    def compute_image_scale(cls, w, h):
+        if w < h:
+            scale = 2000.0 / h
+        else:
+            scale = 2000.0 / w
+
+        return (w * scale, h * scale)
+
+    @classmethod
+    def compute_zoom(cls, canvas_width, canvas_height, image_width, image_height):
+        c_ar = canvas_width / canvas_height
+        i_ar = image_width / image_height
+
+        if c_ar < i_ar:
+            # fit to image width
+            return canvas_width / image_width
+        else:
+            # fit to image height
+            return canvas_height / image_height
+
+    def on_mouseup(self, jsc, x, y, button):
+        """ print out the click coordinate """
+#        global DATA_CLICK_INDEX
+
+        if (button == 0):
+#            print(f'{DATA_CLICK_PREFIX}-{DATA_CLICK_INDEX},{int(x)},{int(y)}')
+#            DATA_CLICK_INDEX = DATA_CLICK_INDEX + 1
+
+            if 'ROOT_RENDER_OBJECT' in jsc.tag:
+                # check if we need to move the tool tip
+                mlp = jsc.eval_js_code(f"""mouse_get_last_position();""")
+                if mlp is not None:
+                    t = time.time()
+                    rolist = jsc.tag['ROOT_RENDER_OBJECT'].point_in_obj(mlp['wx'], mlp['wy'], t)
+
+                    # assemble a layer list
+                    tooltip_idx = None
+                    layer_names = set()
+                    for ro in rolist:
+                        if 'layer_name' in ro.props:
+                            layer_names.add(ro.props['layer_name'])
+                            if 'idx' in ro.props:
+                                tooltip_idx = ro.props['idx']
+
+                    jsc.tag['property_window'] = {'layer_names': layer_names, 'idx': tooltip_idx}
+
+                    html = self.layer_controller.get_tooltip(layer_names, tooltip_idx)
+                    jsc['#properties'].html = html
+
+    def on_options_changed(self, jsc):
+        self.layer_controller.update_options(jsc)
+
+    def on_ready(self, jsc, background_image_path, drawing_context_name, display_context_name):
+        # create the render objects list
+        f = jsc.drawing()
+        f.create_image('img_floor_plan', background_image_path)
+        f.render(jsc)
+
+        # get the canvas width and canvas height
+        canvas_width = jsc.eval_js_code(f'{drawing_context_name}.canvas.width')
+        canvas_height = jsc.eval_js_code(f'{drawing_context_name}.canvas.height')
+
+        # wait for image to load
+        img_width, img_height = canvas_width, canvas_height
+        start_time = time.time()
+        while (time.time() - start_time) < 5:
+            if jsc.eval_js_code('img_floor_plan.complete'):
+                img_width = jsc.eval_js_code('img_floor_plan.naturalWidth')
+                img_height = jsc.eval_js_code('img_floor_plan.naturalHeight')
+                break
+
+        # attempt to scale the image up to a normalized coordinate system of 2000 x 2000
+        img_width, img_height = LayerApp.compute_image_scale(img_width, img_height)
+
+        # calcualte zoom
+        zoom = LayerApp.compute_zoom(canvas_width, canvas_height, img_width, img_height)
+
+        # set the initial zoom to fit the image
+        jsc.eval_js_code(f"""force_zoom({drawing_context_name}.canvas.id, {display_context_name}.canvas.id, 0, 0, {zoom});""")
+        jsc.eval_js_code(f"""force_translate({drawing_context_name}.canvas.id, {display_context_name}.canvas.id, {(canvas_width / zoom - img_width) / 2}, {(canvas_height / zoom - img_height) / 2});""")
+
+        # create the background
+        root_obj = RectObject(flightplan=None, x=0, y=0, width=canvas_width, height=canvas_height, strokeStyle='rgba(0, 0, 0, 0)', fillStyle='rgba(0, 0, 0, 0)', clickable=False)
+
+        # create the background
+        image_obj = ImageObject(name='img', flightplan=None, x=0, y=0, width=img_width, height=img_height, image_name='img_floor_plan', filter_str='opacity(0.2)')
+        root_obj.add_child(image_obj)
+        jsc.tag['ROOT_RENDER_OBJECT'] = root_obj
+
+        # initialize the layer renderers for this jsc
+        for dr in self.layer_controller._layer_datarenderers.values():
+            dr.layer_init(image_obj)
+
+        # generate html for layer selection
+        jsc['#options'].html = self.layer_controller.build_options_html(jsc)
+        self.layer_controller.update_options(jsc)
+
+        # render
+        f.render(jsc)
+
+    def start(self):
+        # start the thread
+        t = threading.Thread(target=self.thread_worker, daemon=True)
+        t.start()
+
+    def thread_worker(self):
+        # init
+        last_render_refresh_time = 0
+        last_status_refresh_time = 0
+        last_tooltip_check_time = 0
+        last_property_refresh_time = 0
+
+        # loop forever
+        while True:
+            t = time.time()
+
+            # refresh the data visually if needed
+            if (t - last_render_refresh_time) > 0.1:
+                for jsc in get_broadcast_jsclients('/'):
+
+                    # refresh the render objects associated with the data
+                    if 'ROOT_RENDER_OBJECT' in jsc.tag:
+                        image_obj = jsc.tag['ROOT_RENDER_OBJECT'].children['img']
+                        self.layer_controller.render(image_obj, jsc.tag['options'])
+
+                        f = JSDraw('ctx_drawing', 'ctx_display')
+                        f.fillStyle = 'white'
+                        f.clear()
+                        jsc.tag['ROOT_RENDER_OBJECT'].render(f, t)
+                        f.render(jsc)
+                last_render_refresh_time = time.time()
+
+
+            if (t - last_tooltip_check_time) > 0.1:
+                for jsc in get_broadcast_jsclients('/'):
+                    if 'ROOT_RENDER_OBJECT' in jsc.tag:
+                        # check if we need to move the tool tip
+                        mlp = jsc.eval_js_code(f"""mouse_get_last_position();""")
+                        if mlp is not None:
+                            if mlp['elapsed_ms'] > 500:
+                                t = time.time()
+                                rolist = jsc.tag['ROOT_RENDER_OBJECT'].point_in_obj(mlp['wx'], mlp['wy'], t)
+
+                                # assemble a layer list
+                                tooltip_idx = None
+                                layer_names = set()
+                                for ro in rolist:
+                                    if 'layer_name' in ro.props:
+                                        layer_names.add(ro.props['layer_name'])
+                                        if 'idx' in ro.props:
+                                            tooltip_idx = ro.props['idx']
+
+                                html = self.layer_controller.get_tooltip(layer_names, tooltip_idx)
+                                if html != '':
+                                    jsc['#tooltip'].html = html
+                                    jsc['#tooltip'].css.left = mlp['px']
+                                    jsc['#tooltip'].css.top = mlp['py']
+                                    jsc['#tooltip'].css.visibility = 'visible'
+                                    jsc['body'].css.cursor = 'crosshair'
+                                else:
+                                    jsc['#tooltip'].css.visibility = 'hidden'
+                                    jsc['body'].css.cursor = 'default'
+                            else:
+                                jsc['#tooltip'].css.visibility = 'hidden'
+                                jsc['body'].css.cursor = 'default'
+                last_tooltip_check_time = time.time()
+
+            # refresh property window if needed
+            if (t - last_property_refresh_time) > 1:
+                for jsc in get_broadcast_jsclients('/'):
+                    if 'property_window' in jsc.tag:
+                        layer_names = jsc.tag['property_window']['layer_names']
+                        idx = jsc.tag['property_window']['idx']
+                        html = self.layer_controller.get_tooltip(layer_names, idx)
+                        jsc['#properties'].html = html
+
+            # refresh the status if needed
+            if (t - last_status_refresh_time) > 1:
+                for jsc in get_broadcast_jsclients('/'):
+                    jsc['#datasources'].html = self.layer_controller.get_datasource_status_messages()
+                last_status_refresh_time = time.time()
+
+            # sleep 10ms
+            time.sleep(0.01)
